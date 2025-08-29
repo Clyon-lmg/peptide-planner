6f38ffa53781e20270d95b37a656aa2f6c973cbb
CLyon-lmg <curtis.lyon@gmail.com>
2025-08-29 13:10:41 -0700

    Dark Mode

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,25 +1,27 @@
-﻿import "server-only";
-import { cookies, headers } from "next/headers";
+﻿﻿import "server-only";
+import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 import type { SupabaseClient } from "@supabase/supabase-js";
 
 export function createServerSupabase(): SupabaseClient {
   const cookieStore = cookies();
-  const headerList = headers();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name) => cookieStore.get(name)?.value,
-        set: (name, value, options) =>
-          cookieStore.set({ name, value, ...options }),
-        remove: (name, options) => cookieStore.delete({ name, ...options }),
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name, value, options) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name, options) {
+          cookieStore.delete({ name, ...options });
+        },
       },
-      headers: {
-        get: (name) => headerList.get(name),
-      },
-    }  );
+    }
+  );
 }
 
 export async function getUserOrNull() {
