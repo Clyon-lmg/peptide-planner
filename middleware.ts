64bddd74ddeb77e617bb2d143ff432622620fb26
CLyon-lmg <curtis.lyon@gmail.com>
2025-08-29 14:26:10 -0700

    Dark Mode

--- middleware.ts
+++ middleware.ts
@@ -36,12 +36,11 @@ export async function middleware(req: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name) => req.cookies.get(name)?.value,
-        set: (name, value, options) =>
-          res.cookies.set({ name, value, ...options }),
-        remove: (name, options) =>
-          res.cookies.set({ name, value: "", ...options, maxAge: 0 }),
-      },
+        getAll: () => req.cookies.getAll(),
+        setAll: (cookies) => {
+          cookies.forEach((cookie) => res.cookies.set(cookie));
+        },
+        },
     }
   );
 
