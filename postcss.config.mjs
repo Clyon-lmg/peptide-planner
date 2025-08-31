c285ceffc6b9479f4c1fcee6a005959261bf9336
CLyon-lmg <curtis.lyon@gmail.com>
2025-08-31 06:43:59 -0700

    Dark Mode

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,9 @@
 import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
-export default { plugins: { tailwindcss, autoprefixer } };
+
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
