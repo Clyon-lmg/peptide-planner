0690ca5eac1b587dd3960120bcbbf7e0d4734ae6
Clyon-lmg <curtis.lyon@gmail.com>
2026-01-02 15:55:11 -0800

    Delete app/auth directory

--- app/auth/signout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { createServerActionSupabase } from "@/lib/supabaseServer";
-
-export async function POST(req: Request) {
-  const supabase = createServerActionSupabase();
-  await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/sign-in", new URL(req.url).origin));
-}
